cccefa1bcdf22df173ff6d927fbcd0a21685198f
zhaojiangbing01 <zhaojiangbing01@58.com>
2026-03-16 14:30:04 +0800

    feature: review add

--- src/langchain.mjs
+++ src/langchain.mjs
@@ -15,3 +15,7 @@ export const multiply = (a, b) => {
 export const divide = (a, b) => {
   return a / b;
 };
+
+export const getWeather = (city) => {
+  return `The weather in ${city} is sunny.`;
+};
